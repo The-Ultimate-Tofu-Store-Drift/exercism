74803acaa93e3c9791f2f7ac40b8dca43497ba4b
Regular Dude <theparceldude@gmail.com>
2017-03-18 18:14:38 +0100

    function encode first build

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,40 @@
+
 def decode():
     pass
 
 
-def encode():
-    pass
+def encode(string):
+
+    encoded = {}
+    counter = 1
+    container = []
+    single = []
+
+    # for i in range(len(string)):
+    #     for j in range(len(string) - 1 - i):
+    #         if string[j] == string[j + 1]:
+
+    for i in string:
+        if i in encoded:
+            encoded[i] += 1
+        else:
+            encoded[i] = 1
+
+    print("encoded:", encoded)
+
+    for i in string:
+        if i not in single:
+            single.append(i)
+
+    print("single: ", single)
+
+    for i in single:
+        container.append(str(encoded[i]) + i)
+
+    print("container: ", container)
+
+    new = "".join(container)
+    print(new)
+
+
+print(encode("AABBBCCCC"))
