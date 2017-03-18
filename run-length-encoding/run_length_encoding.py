b78c7eec395b36f95a3e90f7e2e5af845075f0f0
Regular Dude <theparceldude@gmail.com>
2017-03-18 21:18:14 +0100

    encode function works, added comments and explanations

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -4,23 +4,32 @@ def decode():
 
 
 def encode(string):
-
+    # One dictionary called 'coding' to temporally store the count of letters
+    # and a the list 'container' to store the strings.
     coding = {}
     container = []
 
+    # Checks if the letter is already in the dictionary. If so it increases the key by one.
     for i in string:
         if i in coding:
             coding[i] += 1
         else:
 
+            # If the value for a key is '1', the value will be changed to '!'.
+            # This makes is possible to get rid of them by replacing them with ''.
+            # It changes the dictionary. This is important to know.
             for key, value in coding.items():
                 if value == 1:
                     coding[key] = "!"
 
+            # It takes the key and value and transforms them in strings.
+            # After that combines both and appends the string in the 'container'.
             for key, value in coding.items():
                 container.append(str(value) + str(key))
-            coding.clear()
-            coding[i] = 1
+            coding.clear()  # Deletes everything in the dictionary
+            coding[i] = 1   # Creates a new entry for the letter
+    # When the for-loop is done, there is still a entry in 'coding' left.
+    # This 'else' block does exactly the same thing like the 'for' block before but it transfers the last entry.
     else:
 
         for key, value in coding.items():
@@ -31,13 +40,7 @@ def encode(string):
             container.append(str(value) + str(key))
         coding.clear()
 
-    print("container: ", container)
-    print("coding:", coding)
-
-    encoded_string = "".join(container)
-    encoded_string = encoded_string.replace("!", "")
+    encoded_string = "".join(container)                 # makes a string out of the list 'container'
+    encoded_string = encoded_string.replace("!", "")    # replaces all '!' with ''
 
     return encoded_string
-
-
-print(encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"))
