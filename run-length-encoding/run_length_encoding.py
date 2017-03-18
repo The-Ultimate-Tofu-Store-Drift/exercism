92beeda2963b70c4f087c6f75d5b1ccd69e659f4
Regular Dude <theparceldude@gmail.com>
2017-03-18 20:28:49 +0100

    changed code, now it works as intended

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -8,28 +8,39 @@ def encode(string):
     coding = {}
     container = []
     single_letters = []
+    test = []
 
     for i in string:
         if i in coding:
             coding[i] += 1
         else:
+            for key, value in coding.items():
+                test.append(str(value) + str(key))
+            coding.clear()
             coding[i] = 1
+    else:
+        for key, value in coding.items():
+            test.append(str(value) + str(key))
+        coding.clear()
 
+    print("test: ", test)
     print("coding:", coding)
 
-    for i in string:
-        if i not in single_letters:
-            single_letters.append(i)
+    # for i in string:
+    #     if i not in single_letters:
+    #         single_letters.append(i)
 
     print("single_letters: ", single_letters)
 
-    for i in single_letters:
-        container.append(str(coding[i]) + i)
+    # for i in single_letters:
+    #     container.append(str(coding[i]) + i)
 
     print("container: ", container)
 
     encoded_string = "".join(container)
     print(encoded_string)
 
+    new = "".join(test)
+    print(new)
 
-print(encode("AABBBCCCC"))
+print(encode("AAFBBBCFFCCC"))
