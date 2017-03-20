07ae86d953cee1afa005361f45e49f0ebff5c12f
Regular Dude <theparceldude@gmail.com>
2017-03-20 15:04:39 +0100

    trying various things out

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,2 +1,38 @@
-def meetup_day():
-    pass
+from datetime import date
+import calendar
+
+
+def meetup_day(year, month, weekday, spec):
+
+    teenth = [i for i in range(13, 18)]
+    days = {
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6
+    }
+
+    # if spec == "teenth":
+
+    t = []
+
+    b = calendar.monthcalendar(year, month)
+
+    for i in range(len(b)):
+        for j in b[i]:
+            print(j)
+
+    a = date.isocalendar(date.today())
+    print(teenth)
+    print(t)
+
+    return b
+
+
+print(meetup_day(2013, 5, 'Monday', 'teenth'))  # 2013, 5, 13
+
+# teenth : 13 - 17
+# Monday : 0; Sunday: 6
