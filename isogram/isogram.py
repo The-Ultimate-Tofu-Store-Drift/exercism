550eaec1f83c32f85c72c9bc66419f75adc27761
Regular Dude <theparceldude@gmail.com>
2017-04-02 10:44:01 +0200

    works as indented

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,22 @@
-def is_isogram():
-    pass
+
+def is_isogram(phrase):
+
+    phrase = phrase.lower()
+    phrase = phrase.replace(" ", "")
+    phrase = phrase.replace("-", "")
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet = list(alphabet)
+
+    temp = []
+
+    for i in phrase:
+        if i in alphabet:
+            temp.append(i)
+            del alphabet[alphabet.index(i)]
+
+        elif i not in alphabet:
+            return False
+
+    if "".join(temp) == phrase:
+        return True
