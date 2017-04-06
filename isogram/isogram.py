0f87f15fe1897db0a8065370d4991be97d962a7f
Regular Dude <theparceldude@gmail.com>
2017-04-06 15:06:44 +0200

    changed the whole function to a much better one

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,22 +1,7 @@
+# This is the solution from the user 'j2towers'
 
 def is_isogram(phrase):
-
-    phrase = phrase.lower()
-    phrase = phrase.replace(" ", "")
-    phrase = phrase.replace("-", "")
-
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    alphabet = list(alphabet)
-
-    temp = []
-
-    for i in phrase:
-        if i in alphabet:
-            temp.append(i)
-            del alphabet[alphabet.index(i)]
-
-        elif i not in alphabet:
+    for i in phrase.lower():
+        if phrase.lower().count(i) > 1 and phrase.isalpha():
             return False
-
-    if "".join(temp) == phrase:
-        return True
+    return True
