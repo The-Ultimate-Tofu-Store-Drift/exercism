11da344036e74e270ba9234ff18cd52a9e26cfcd
Regular Dude <theparceldude@gmail.com>
2017-04-06 15:20:53 +0200

    changed the whole function to a much better one

--- bob/bob.py
+++ bob/bob.py
@@ -1,39 +1,13 @@
+# This solution is from the user 'j2towers'
 
-def hey(what):
+def hey(saidtobob):
+    saidtobob = saidtobob.rstrip()
 
-    # This replaces the '\t'. Yeah, I know.
-    what = what.replace("\t", "")
-
-    # This block is necessary for checking.
-    special = [".", "!", "?", "%", "*", "^", "(", "@", "#", "$", "1", "2", "3", " ", ",", ""]
-    numbers = ["1", "2", "3", "4"]
-    empty = ["", " ", "    "]
-
-    # This checks if the string contains characters from the variable 'empty'.
-    if what[:] in empty:
-        return "Fine. Be that way!"
-
-    # It checks if the characters are not in 'special'. If so, it appends them to 'new_list'.
-    new_list = []
-    l = list(what)
-    for i in l:
-        if i not in special:
-            new_list.append(i)
-
-    # It just joins the content of 'new_list' to a string.
-    new_list = "".join(new_list)
-
-    # If the last character is ' ' it replaces all of them with ''.
-    if what[-1] == " ":
-        what = what.replace(" ", "")
-
-    # This one checks if the character in 'new_list' are all uppercase-letters and are not in 'numbers'
-    if new_list[:] == new_list[:].upper() and new_list[:] != "" and new_list not in numbers:
+    if saidtobob.isupper():
         return "Whoa, chill out!"
-
-    # Checks if the last character is '?'.
-    elif what[-1] == "?":
+    elif saidtobob.endswith("?"):
         return "Sure."
-
+    elif saidtobob.isspace() or saidtobob == "":
+        return "Fine. Be that way!"
     else:
         return "Whatever."
