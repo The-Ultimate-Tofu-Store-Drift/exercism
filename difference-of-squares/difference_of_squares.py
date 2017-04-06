ad139175b2b1c814acad6c014682c9d8fbc824a2
Regular Dude <theparceldude@gmail.com>
2017-04-06 15:29:45 +0200

    wrote the code

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,24 @@
-def square_of_sum():
-    pass
+
+def square_of_sum(n):
+
+    su = 0
+
+    for i in range(n + 1):
+        su += i
+
+    su *= su
+    return su
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(n):
+
+    su = 0
+
+    for i in range(n + 1):
+        su = su + (i * i)
+
+    return su
 
 
-def difference():
-    pass
+def difference(n):
+    return square_of_sum(n) - sum_of_squares(n)
