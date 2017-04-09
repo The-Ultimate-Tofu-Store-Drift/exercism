51b7342e10dfd24656d53779b9efd4088d8a3ab6
Regular Dude <theparceldude@gmail.com>
2017-04-09 07:59:02 +0200

    another way

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,23 @@
-def detect_anagrams():
-    pass
+
+def detect_anagrams(word, list_with_words):
+
+    counter = 0
+    results = []
+
+    while counter != len(word):
+
+        for i in list_with_words:
+            if len(word) == len(i):
+                for letter in list_with_words[i]:
+                    if list_with_words.lower.count(letter) == word.lower.count(letter):
+                        counter += 1
+                    else:
+                        break
+            else:
+                break
+    else:
+        results.append(list_with_words[i])
+
+
+if __name__ == "__main__":
+    print(detect_anagrams())
