bad93a0cd8eb0516503f247e5e2bbbff631be639
Regular Dude <theparceldude@gmail.com>
2017-04-09 08:02:36 +0200

    fix bugs

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -6,10 +6,10 @@ def detect_anagrams(word, list_with_words):
 
     while counter != len(word):
 
-        for i in list_with_words:
-            if len(word) == len(i):
+        for i in range(len(list_with_words)):
+            if len(word) == len(list_with_words[i]):
                 for letter in list_with_words[i]:
-                    if list_with_words.lower.count(letter) == word.lower.count(letter):
+                    if list_with_words.count(letter).lower == word.lower.count(letter):
                         counter += 1
                     else:
                         break
@@ -18,6 +18,8 @@ def detect_anagrams(word, list_with_words):
     else:
         results.append(list_with_words[i])
 
+    return results
+
 
 if __name__ == "__main__":
-    print(detect_anagrams())
+    print(detect_anagrams("ant", ["tan", "stand", "at"]))
