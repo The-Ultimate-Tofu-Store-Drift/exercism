483619070dffde1883e9adbd7f00a6d210082880
Regular Dude <theparceldude@gmail.com>
2017-04-09 08:33:33 +0200

    it's alive

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -4,22 +4,22 @@ def detect_anagrams(word, list_with_words):
     counter = 0
     results = []
 
-    while counter != len(word):
+    for i in range(len(list_with_words)):
 
-        for i in range(len(list_with_words)):
-            if len(word) == len(list_with_words[i]):
-                for letter in list_with_words[i]:
-                    if list_with_words[i].lower.count(letter) == word.lower.count(letter):
-                        counter += 1
-                    else:
-                        break
-            else:
-                break
-    else:
-        results.append(list_with_words[i])
+        if len(word) == len(list_with_words[i]) and word.lower() != list_with_words[i].lower():
+
+            for letter in list_with_words[i]:
+
+                if list_with_words[i].lower().count(letter) == word.lower().count(letter):
+
+                    counter += 1
+
+                    if counter == len(word):
+                        results.append(list_with_words[i])
+                        counter = 0
+
+                else:
+                    counter = 0
+                    break
 
     return results
-
-
-if __name__ == "__main__":
-    print(detect_anagrams("ant", ["tan", "stand", "at"]))
