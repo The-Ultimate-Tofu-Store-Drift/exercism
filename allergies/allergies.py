fd473c5ac655a7956b654bf68004365833275557
Regular Dude <theparceldude@gmail.com>
2017-04-10 12:39:34 +0200

    wrote the code

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,3 +1,40 @@
+
 class Allergies(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, points):
+        self.points = points
+
+    list_of_items = {
+        "eggs": 1,
+        "peanuts": 2,
+        "shellfish": 4,
+        "strawberries": 8,
+        "tomatoes": 16,
+        "chocolate": 32,
+        "pollen": 64,
+        "cats": 128
+    }
+
+    order = ["cats", "pollen", "chocolate", "tomatoes", "strawberries", "shellfish", "peanuts", "eggs"]
+    lst_results = []
+
+    def is_allergic_to(self, item):
+        if self.list_of_items[item] <= self.points:
+            return True
+        else:
+            return False
+
+    def lst(self):
+        while self.points > 256:
+            self.points -= 256
+
+        for i in self.order:
+            if self.list_of_items[i] <= self.points:
+                self.lst_results.append(i)
+                self.points -= self.list_of_items[i]
+
+        return self.lst_results[::-1]
+
+if __name__ == "__main__":
+    mark = Allergies(509)
+    print(mark.lst())
