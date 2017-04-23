f2672a1010994c1826e7e907cdbbbfde1ffe3be8
Regular Dude <theparceldude@gmail.com>
2017-04-23 09:07:00 +0200

    the code works

--- series/series.py
+++ series/series.py
@@ -1,2 +1,20 @@
-def slices():
-    pass
+
+def slices(string, digit):
+
+    temp = []
+    result = []
+
+    if digit > len(string) or digit == 0:
+        raise ValueError
+
+    for i in range(len(string)):
+        if len(string[i:i + digit]) == digit:
+            temp.append(string[i:i + digit])
+
+    for i in range(len(temp)):
+        result.append([])
+
+        for j in temp[i]:
+            result[i].append(int(j))
+
+    return result
