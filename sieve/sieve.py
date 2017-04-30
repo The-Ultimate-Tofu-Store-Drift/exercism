68fcd7ed7cc2fe46951c7704c47d9b14cfc94081
Regular Dude <theparceldude@gmail.com>
2017-04-30 12:12:33 +0200

    it's alive, the runtime is bad, though

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,22 @@
-def sieve():
-    pass
+
+def sieve(n):
+
+    if n < 2:
+        return []
+    elif n == 2:
+        return [2]
+
+    sieve_list = [i for i in range(2, n + 1)]
+
+    for number in sieve_list:
+        checker = number
+
+        for i in sieve_list:
+            checker += number
+
+            for j in sieve_list:
+
+                if j == checker:
+                    del sieve_list[sieve_list.index(j)]
+
+    return sieve_list
