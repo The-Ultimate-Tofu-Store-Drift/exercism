3712e8101774d76209cf6f0b438a56640e1c6af6
Regular Dude <theparceldude@gmail.com>
2017-05-01 11:14:38 +0200

    wrote the code

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,53 @@
-def encode():
-    pass
+
+def encode(string_input):
+
+    plain_key = "abcdefghijklmnopqrstuvwxyz"
+    cipher_key = plain_key[::-1]
+
+    temp = []
+
+    for i in range(len(string_input)):
+
+        if string_input[i].lower().isalpha():
+            temp.append(cipher_key[plain_key.index(string_input[i].lower())])
+
+        elif string_input[i].isnumeric():
+            temp.append(string_input[i])
+
+    output = []
+    counter = 0
+
+    for i in range(len(temp)):
+
+        output.append(temp[i])
+        counter += 1
+
+        if counter == 5:
+
+            if i + 1 != len(temp):
+                output.append(" ")
+
+            counter = 0
+
+    return "".join(output)
 
 
-def decode():
-    pass
+def decode(string_input):
+
+    plain_key = "abcdefghijklmnopqrstuvwxyz"
+    cipher_key = plain_key[::-1]
+
+    temp = []
+
+    for i in range(len(string_input)):
+
+        if string_input[i].isalpha():
+            temp.append(plain_key[cipher_key.index(string_input[i])])
+
+        elif string_input[i].isnumeric():
+            temp.append(string_input[i])
+
+    return "".join(temp)
+
+if __name__ == "__main__":
+    print(decode(input("string: ")))
