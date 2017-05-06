d56764f24cee7013c37eb10a066ccde8b30c6fe6
Regular Dude <theparceldude@gmail.com>
2017-05-06 09:00:57 +0200

    wrote the code

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,15 @@
-def sum_of_multiples():
-    pass
+
+def sum_of_multiples(n, particular_numbers):
+
+    range_of_numbers = [i for i in range(0, n)]
+    result = []
+
+    for number in particular_numbers:
+        for range_number in range_of_numbers:
+
+            if range_number % number == 0 and range_number not in result:
+                result.append(range_number)
+
+    result = sum(result)
+
+    return result
